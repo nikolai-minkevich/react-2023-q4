fb8a23b631bba06e68dd62920e543992ede1b83f
Nikolai Minkevich <n.minkevich@loyaltyplant.com>
2023-10-28 00:01:13 +0200

    feat: implement interface ICard and cards props

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import './Content.css';
 import Card from '../Card';
+import ICard from '../../interfaces/ICard';
+import Loader from '../Loader';
 
 type ContentProps = {
-  message: string;
+  cards: ICard[];
 };
 type MyState = {
   count: number;
 };
+
 class Content extends React.Component<ContentProps, MyState> {
   state: MyState = {
     count: 0,
   };
+
   render() {
+    if (!this.props.cards || this.props.cards.length === 0) {
+      return <Loader></Loader>;
+    }
+
     return (
       <div>
-        <Card title="Ti" description="desc"></Card>
+        {this.props.cards.map((card) => (
+          <Card title={card.title} description={card.description}></Card>
+        ))}
       </div>
     );
   }
--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,15 +2,20 @@ import Nav from '../Nav';
 import Content from '../Content';
 import './Page.css';
 import React from 'react';
+import ICard from '../../interfaces/ICard';
+
+const cards: ICard[] = [{ title: '131', description: '22' }];
 
 class Page extends React.Component {
   render() {
     return (
-      <div className="page">
-        <Nav message="111"></Nav>
+      <>
+        <div className="page">
+          <Nav message="111"></Nav>
 
-        <Content message="222"></Content>
-      </div>
+          <Content cards={cards}></Content>
+        </div>
+      </>
     );
   }
 }

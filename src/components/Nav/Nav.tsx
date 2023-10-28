22e95fc9152c696f5e82fe035900dfb7d7caba5a
Nikolai Minkevich <n.minkevich@loyaltyplant.com>
2023-10-28 02:08:54 +0200

    feat: add logo

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Nav.css';
 import Search from '../Search';
+import Logo from '../Logo';
 
 class Navbar extends React.Component {
   render() {
     return (
       <>
         <div className="nav">
-          <div className="nav__logo">Logo</div>
+          <div className="nav__logo">
+            <Logo></Logo>
+          </div>
           <div className="nav__search">
             <Search message="Search"></Search>
           </div>

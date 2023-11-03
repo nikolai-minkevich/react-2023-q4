0cadefc11d973bb63d9144c14b45a4be28414bff
Nikolai Minkevich <n.minkevich@loyaltyplant.com>
2023-11-03 21:04:15 +0100

    refactor: improve quality of interfaces

--- src/interfaces/IEpisode.tsx
+++ src/interfaces/IEpisode.tsx
@@ -1,40 +1,16 @@
+import ISeason from './ISeason';
+import ISeries from './ISeries';
+
 interface IEpisode {
   uid: string;
-  title: string;
-  seasonNumber: number;
-  episodeNumber: number;
-  yearFrom: number;
-  yearTo: number;
-  usAirDate: string;
+  title: string | null;
+  series: ISeries | null;
+  season: ISeason | null;
+  seasonNumber: number | null;
+  episodeNumber: number | null;
+  yearFrom: number | null;
+  yearTo: number | null;
+  usAirDate: string | null;
 }
 
 export default IEpisode;
-
-/**
- * [
-    {
-      "uid": "EPMA0000001002",
-      "title": "'Til Death Do Us Part",
-      "titleGerman": "Bis daß der Tod uns scheide",
-      "titleItalian": null,
-      "titleJapanese": null,
-      "series": {
-        "uid": "SEMA0000073238",
-        "title": "Star Trek: Deep Space Nine"
-      },
-      "season": {
-        "uid": "SAMA0000001741",
-        "title": "DS9 Season 7"
-      },
-      "seasonNumber": 7,
-      "episodeNumber": 18,
-      "productionSerialNumber": "40510-568",
-      "featureLength": false,
-      "stardateFrom": null,
-      "stardateTo": null,
-      "yearFrom": 2375,
-      "yearTo": 2375,
-      "usAirDate": "1999-04-14",
-      "finalScriptDate": null
-    },
- */

b7824653a9188df549a7ecb05f6a344d13fd95b2
Nikolai Minkevich <n.minkevich@loyaltyplant.com>
2023-12-03 21:58:59 +0100

    feat: integrate yup validations

--- src/routes/new-form.tsx
+++ src/routes/new-form.tsx
@@ -6,12 +6,24 @@ import '../styles/form.css';
 import { useDispatch } from 'react-redux';
 import { setData } from '../appSlice';
 import { Link } from 'react-router-dom';
+
 import IFormData from '../interfaces/form-data';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+// yup schema
+const schema = yup.object().shape({
+  name: yup.string().required('Name is a required field'),
+});
 
 export const Form: React.FC = () => {
   const dispatch = useDispatch();
-
-  const { register, handleSubmit } = useForm<IFormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>({ resolver: yupResolver(schema) });
 
   const onSubmit = (data: IFormData) => {
     console.log(data);
@@ -24,6 +36,7 @@ export const Form: React.FC = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Name</label>
         <input {...register('name')} />
+        {errors.name && <p>{errors.name.message}</p>}
 
         <input type="submit" />
       </form>
